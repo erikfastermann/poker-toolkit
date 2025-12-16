1056e22adf9fed42e4e0eb9b4e9ee30ff67f67da
Erik Fastermann <erik@fastermann.de>
2025-12-17 00:58:14 +0100

    const compute hands array

--- poker-core/src/init.rs
+++ poker-core/src/init.rs
@@ -1,12 +1,11 @@
 use std::sync::Once;
 
-use crate::{cards::Cards, hand::Hand};
+use crate::cards::Cards;
 
 static INIT: Once = Once::new();
 
 pub unsafe fn init() {
     INIT.call_once(|| {
         Cards::init();
-        Hand::init();
     });
 }
